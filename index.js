e8070e0249f10fdf5e5785488574c247a370e994
Adit <triansyah.a17@gmail.com>
2022-07-20 19:11:56 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+// Soal 1
+const data ={
+ kenapaBelajarProgramming: 'karena saya ingin belajar tentang komputer lebih dalam termasuk programming karna saya sebelum nya tidak terlalu sering memaki laptop/komputer',
+ motivasiAtauTujuannyaApa: 'karena saya ingin terus belajar dalam pemrogaman dan ingin berkembang dalam bidang teknologi dan ingin menjungjung tinggi dalam bidang programming',
+ targetnyaApa: 'tujuan dari itu menjadi yg lebih baik dari yaang lain dan menjadi lebih baik dan lebih baik',
+ bagaimanakalianBelajarprogramming: 'belajar dengan lebih giat dan tekun dan bersungguh-sungguh dalam belajar dan menjungjung tinggi'
+}
+console.log (data)
+
+// Soal 2
+const rate ={
+SebutkanLangkahBelajarProgramming: '1.pilih bahasa pemrogaman yang ingin di pelajari dan kuasai',
+langkah: '2. Gunakan bantuan modul atau bahan bacaan pemrograman dengan tutorial lengkap',
+langkah: '3. Buat dan susun jadwal belajar pemrograman',
+langkah: '4. Lakukan praktek sebanyak-banyaknya',
+langkah: '5. Hindari melakukan copy paste',
+langkah: '6. Lakukan perbaikan secara mandiri',
+langkah: '7. Melakukan praktek secara konsisten',
+langkah: '8. Istirahat'
+}
+console.log (rate)
+
+// Soal 3
+const key ={
+    sebutakanLangkahJikaKesulitanErorBug: 'jika ada kesulitan maka hadapilah sendiri dan jika tidak bisa maka search google jika eror atau bug juga bisa search saja '
+}
+console.log (key)
+
+// Soal 4-1
+const and ={
+    kesimpulan: 'karena tidak dapat menemukan varialel num',
+    perbaiki: 'harus menambahkan atau mendeklarasian variabel num',
+    contoh: 'let num;'
+}
+console.log ('Tugas1')
+console.log (and)
+
+// soal 4-2
+const red ={
+    kesimpulan: 'jika suatu variabel telah dideklarasikan, tetapi belum diberi nilai, secara otomatis diberi nilai yang tidak ditentukan',
+    perbaiki: 'kehilangan inisialisasi array. Jadi, perlu menginisialisasi array terlebih dahulu',
+}
+console.log ('Tugas2')
+console.log (red)
+
+// soal 4-3
+const blue ={
+kesimpulan: 'jadi ketika {} pertama harus memakai (,). tetapi jika {} akhir bisa pakai bisa juga tidak',
+}
+// contoh perbaiki
+const movies = [
+    {title: 'mortal engines', category: 'action'},
+    {title: 'underwater', category: 'action'}
+]
+console.log ('Tugas3')
+    console.log (blue)
